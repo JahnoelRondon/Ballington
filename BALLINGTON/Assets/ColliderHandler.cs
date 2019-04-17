f7ab4eaa3d06e7f03f0a53de9b7f8f1949fb0957
jahnoelrondon <jahnoelrondon03@gmail.com>
2019-04-17 12:24:34 -0400

    Recovered Player
    recored players movement and triggering

---
+++ BALLINGTON/Assets/ColliderHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderHandler : MonoBehaviour
+{
+    private PlayerMover _player;
+
+    void Start()
+    {
+        _player = GetComponent<PlayerMover>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "jumpAble":
+                _player.isGrounded = true;
+                break;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "jumpAble":
+                _player.isGrounded = false;
+                break;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Death":
+                _player.rb.velocity = Vector3.zero;
+                transform.position = new Vector3(9f, 111f, -140f);
+                break;
+        }
+    }
+}
---
+++ BALLINGTON/Assets/PlayerMover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+
+    //private CharacterController _Controller;
+
+    public float playerspd = 300f;
+
+    public Rigidbody rb;
+    public float jumpForce = 1f;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        //_Controller = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+    }
+
+
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = movement * playerspd;
+
+        rb.AddForce(velocity * Time.deltaTime);
+    }
+
+
+
+}
