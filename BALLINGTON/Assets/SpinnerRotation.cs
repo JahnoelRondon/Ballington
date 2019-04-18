c0998a9c428efd0764793a5a02719a69fbfce32f
jahnoelrondon <jahnoelrondon03@gmail.com>
2019-04-18 10:50:08 -0400

    Added Spinner Obstacle / Before editing error

---
+++ BALLINGTON/Assets/SpinnerRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerRotation : MonoBehaviour
+{
+    [SerializeField] float spinSpd = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate (new Vector3(0, 1, 0) * spinSpd * Time.deltaTime);
+    }
+}
