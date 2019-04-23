60539035110803b4f45091fefc588af79b02a9a5
jahnoelrondon <jahnoelrondon03@gmail.com>
2019-04-23 09:43:18 -0400

    Finished MainMenu

---
+++ BALLINGTON/Assets/MainUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainUI : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("World1");
+    }
+
+    
+
+    public void Quit()
+    {
+        Application.OpenURL("https://jayyron.itch.io/");
+    }
+
+}
