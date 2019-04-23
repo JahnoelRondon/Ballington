09a5099ea80078a9a4df925049066bee16681555
jahnoelrondon <jahnoelrondon03@gmail.com>
2019-04-23 10:50:48 -0400

    Added pause menu setup halfway done.
    going into pausing

---
+++ BALLINGTON/Assets/PauseUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseUI : MonoBehaviour
+{
+    public void goToMain()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+
+    public void pauseQuit()
+    {
+        Application.OpenURL("https://jayyron.itch.io/");
+    }
+}
